1ee98335af2653e21f6ed7910f3941d1df7bd011
Nishwa Tuniki <tunikinishwa@gmail.com>
2025-09-02 11:48:55 -0500

    Update LiveCam.js

--- traffic-violation-site/src/Components/LiveCam.js
+++ traffic-violation-site/src/Components/LiveCam.js
@@ -37,7 +37,7 @@ const LiveCam = () => {
       return;
     }
 
- const ws = new WebSocket(`${process.env.REACT_APP_WS_URL}/live-video`);
+ const ws = new WebSocket(`${process.env.REACT_APP_API_URL.replace(/^http/, "ws")}/live-video`);
 
     ws.onopen = () => {
       console.log("WebSocket connection established");
